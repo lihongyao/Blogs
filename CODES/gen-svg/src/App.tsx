6e3c992f3b4c11b2652700cec6bafd01e48ee08c
leo <leo@8u8.cc>
2025-10-15 10:02:37 +0400

    docs: 文档更新

--- CODES/gen-svg/src/App.tsx
+++ CODES/gen-svg/src/App.tsx
@@ -1,9 +1,23 @@
-import Icon from "./components/IconSvg";
+import IconSvg from "./components/IconSvg";
 export default function App() {
   return (
-    <div className="p-20 flex items-center gap-4">
-      <Icon name="profile/orders" size={32} color="#4F46E5" />
-      <Icon name="profile/orders" className="size-8 text-red-500" />
+    <div className="p-20 flex flex-col gap-6">
+      {/* 默认 */}
+      <IconSvg name="wx" color="var(--text-primary-color)" />
+      {/* 尺寸 */}
+      <div className="flex items-center gap-4">
+        <IconSvg name="profile/orders" style={{ width: 24, height: 24 }} />
+        <IconSvg name="profile/orders" size={24} />
+        <IconSvg name="profile/orders" className="w-6 h-6" />
+        <IconSvg name="profile/orders" className="size-6" />
+      </div>
+      {/* 颜色 */}
+      <div className="flex items-center gap-4">
+        <IconSvg name="tiktok" style={{ color: "blue" }} />
+        <IconSvg name="tiktok" color="red" />
+        <IconSvg name="tiktok" color="var(--text-primary-color)" />
+        <IconSvg name="tiktok" className=" fill-amber-600" />
+      </div>
     </div>
   );
 }
--- CODES/gen-svgs/src/components/svgPath_all.ts
+++
@@ -1,5 +0,0 @@
-// ⚠️ 此文件由脚本自动生成，请勿手动修改
-// 生成时间: 2025-10-04T22:14:45.917Z
-export const SVG_PATH_NAMES = ["profile/orders", "tiktok"] as const;
-
-export type SvgPathName = (typeof SVG_PATH_NAMES)[number];
